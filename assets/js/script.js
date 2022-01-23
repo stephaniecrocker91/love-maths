45b29cbcc82961de4364914ce04a12b796cddc7e
Stephie <stephaniecrocker91@gmail.com>
2022-01-23 16:04:58 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
-
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition");
-
 });
 
 /**
@@ -26,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Created two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
