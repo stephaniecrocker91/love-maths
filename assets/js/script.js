f1b2fe9a9f089ff0ebca27e7f9a95b80b84670cf
Stephie <stephaniecrocker91@gmail.com>
2022-01-23 10:38:19 +0000

    Display addition questions to user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,33 @@ document.addEventListener("DOMContentLoaded", function() {
                  alert("You clicked Submit!");
              } else {
                  let gameType = this.getAttribute("data-type");
-                 alert(`You clicked ${gameType}`);
+                 runGame(gameType);
              }
-         })
+         });
      }
+
+     runGame("addition");
+
 })
 
+/**
+ * The main game "loop", called when the script is first loaded and after the user's answer has been processed.
+ */
 
 
 
+function runGame(gameType) {
 
-function runGame() {
+    // Created two random numbers between 1 and 25
+    let num1= Math.floor(Math.random() * 25) +1;
+    let num2= Math.floor(Math.random() * 25) +1;
+    
+    if (gameType==="addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Abprting!`;
+    }
 
 }
 
@@ -40,7 +56,12 @@ function incrementWrongScore() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
+
 
 }
 
